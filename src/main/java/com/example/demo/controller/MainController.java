635129a127773658a1a8419f73f83fe4a8423650
SantoshVarmaAlluri <allurisantoshvarma8@gmail.com>
2022-02-12 11:12:34 -0600

    Updated search by Id service

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,10 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.model.Repository;
 import com.example.demo.model.SchemaModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MainController {
